04452a5fad593617d9ea83b1f716caaeacfb014b
imsyy <one@imsyy.top>
2023-04-24 16:02:04 +0800

    feat: 路由遍历

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,6 @@
+const fs = require("fs");
+const path = require("path");
 const Router = require("koa-router");
-
-const bilibiliRouter = require("./bilibili");
-const zhihuRouter = require("./zhihu");
-const baiduRouter = require("./baidu");
-const weiboRouter = require("./weibo");
-const itHomeRouter = require("./ithome");
-const krRouter = require("./36kr");
-const sspaiRouter = require("./sspai");
-const tiebaRouter = require("./tieba");
-const toutiaoRouter = require("./toutiao");
-const thepaperRouter = require("./thepaper");
-const juejinRouter = require("./juejin");
-const newsqqRouter = require("./newsqq");
 
 const router = new Router();
 
@@ -20,18 +9,17 @@ router.get("/", async (ctx) => {
   await ctx.render("index");
 });
 
-router.use(bilibiliRouter.routes());
-router.use(zhihuRouter.routes());
-router.use(baiduRouter.routes());
-router.use(weiboRouter.routes());
-router.use(itHomeRouter.routes());
-router.use(krRouter.routes());
-router.use(sspaiRouter.routes());
-router.use(tiebaRouter.routes());
-router.use(toutiaoRouter.routes());
-router.use(thepaperRouter.routes());
-router.use(juejinRouter.routes());
-router.use(newsqqRouter.routes());
+// 遍历所有路由模块
+fs.readdirSync(__dirname)
+  .filter((filename) => filename.endsWith(".js") && filename !== "index.js")
+  .forEach((filename) => {
+    const routerPath = path.join(__dirname, filename);
+    const routerModule = require(routerPath);
+    // 自动注册路由
+    if (routerModule instanceof Router) {
+      router.use(routerModule.routes());
+    }
+  });
 
 // 404 路由
 router.use(async (ctx) => {
