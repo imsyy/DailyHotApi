9e9fc713de68ed70bab7ad648bd7cc22c3d8e95a
底层用户 <one@imsyy.top>
2023-07-12 17:02:46 +0800

    Update and rename douyin2.js to douyin_new.js

--- routes/douyin_new.js
+++ routes/douyin_new.js
@@ -1,18 +1,24 @@
 /*
- * @Descripttion:
- * @version:
- * @Author: WangPeng
- * @Date: 2023-07-10 16:56:01
- * @LastEditors: WangPeng
- * @LastEditTime: 2023-07-12 15:09:39
+ * @version: 1.0.0
+ * @author: WangPeng
+ * @date: 2023-07-10 16:56:01
+ * @customEditors: imsyy
+ * @lastEditTime: 2023-07-11 16:54:38
  */
+
 const Router = require("koa-router");
-const douyinRouter = new Router();
+const douyinNewRouter = new Router();
 const axios = require("axios");
 const { get, set, del } = require("../utils/cacheData");
 
+// 接口信息
+const routerInfo = {
+  title: "抖音",
+  subtitle: "热点榜",
+};
+
 // 缓存键名
-const cacheKey = "douyinData";
+const cacheKey = "douyinHotNewData";
 
 // 调用时间
 let updateTime = new Date().toISOString();
@@ -20,43 +26,53 @@ let updateTime = new Date().toISOString();
 // 调用路径
 const url = "https://aweme.snssdk.com/aweme/v1/hot/search/list/";
 const HEADERS = {
-  'user-agent': 'okhttp3'
-}
+  "user-agent": "okhttp3",
+};
 const QUERIES = {
-  'device_platform': 'android',
-  'version_name': '13.2.0',
-  'version_code': '130200',
-  'aid': '1128'
-}
+  device_platform: "android",
+  version_name: "13.2.0",
+  version_code: "130200",
+  aid: "1128",
+};
 
 // 数据处理
 const getData = (data) => {
   if (!data) return [];
-  return data.map((v) => {
-    return {
-      id: v.group_id,
-      title: v.word,
-      pic: v.word_cover?.url_list[0] || null,
-      hot: v.hot_value,
-      url: `https://www.douyin.com/hot/${v.sentence_id}`,
-      mobileUrl: `https://www.douyin.com/hot/${v.sentence_id}`,
-    };
-  });
+  try {
+    const jsonObject = data.data.word_list;
+    return jsonObject.map((v) => {
+      return {
+        title: v.word,
+        pic: `${v.word_cover.url_list[0]}`,
+        hot: Number(v.hot_value),
+        url: `https://www.douyin.com/hot/${encodeURIComponent(v.sentence_id)}`,
+        mobileUrl: `https://www.douyin.com/hot/${encodeURIComponent(
+          v.sentence_id
+        )}`,
+      };
+    });
+  } catch (error) {
+    console.error("数据处理出错" + error);
+    return [];
+  }
 };
 
-// 抖音热榜
-douyinRouter.get("/douyin2", async (ctx) => {
-  console.log("获取抖音热榜");
+// 抖音热点榜
+douyinNewRouter.get("/douyin_new", async (ctx) => {
+  console.log("获取抖音热点榜");
   try {
     // 从缓存中获取数据
     let data = await get(cacheKey);
     const from = data ? "cache" : "server";
     if (!data) {
       // 如果缓存中不存在数据
-      console.log("从服务端重新获取抖音热榜");
+      console.log("从服务端重新获取抖音热点榜");
       // 从服务器拉取数据
-      const response = await axios.get(url, { headers: HEADERS, params: QUERIES });
-      data = getData(response.data.data.word_list);
+      const response = await axios.get(url, {
+        headers: HEADERS,
+        params: QUERIES,
+      });
+      data = getData(response.data);
       updateTime = new Date().toISOString();
       // 将数据写入缓存
       await set(cacheKey, data);
@@ -64,8 +80,7 @@ douyinRouter.get("/douyin2", async (ctx) => {
     ctx.body = {
       code: 200,
       message: "获取成功",
-      title: "抖音",
-      subtitle: "热榜",
+      ...routerInfo,
       from,
       total: data.length,
       updateTime,
@@ -75,29 +90,30 @@ douyinRouter.get("/douyin2", async (ctx) => {
     console.error(error);
     ctx.body = {
       code: 500,
-      title: "抖音",
-      subtitle: "热榜",
+      ...routerInfo,
       message: "获取失败",
     };
   }
 });
 
-// 抖音热榜 - 获取最新数据
-douyinRouter.get("/douyin2/new", async (ctx) => {
-  console.log("获取抖音热榜 - 最新数据");
+// 抖音热点榜 - 获取最新数据
+douyinNewRouter.get("/douyin_new/new", async (ctx) => {
+  console.log("获取抖音热点榜 - 最新数据");
   try {
     // 从服务器拉取最新数据
-    const response = await axios.get(url, { headers: HEADERS, params: QUERIES });
-    const newData = getData(response.data.word_list);
+    const response = await axios.get(url, {
+      headers: HEADERS,
+      params: QUERIES,
+    });
+    const newData = getData(response.data);
     updateTime = new Date().toISOString();
-    console.log("从服务端重新获取抖音热榜");
+    console.log("从服务端重新获取抖音热点榜");
 
     // 返回最新数据
     ctx.body = {
       code: 200,
       message: "获取成功",
-      title: "抖音",
-      subtitle: "热榜",
+      ...routerInfo,
       total: newData.length,
       updateTime,
       data: newData,
@@ -115,8 +131,7 @@ douyinRouter.get("/douyin2/new", async (ctx) => {
       ctx.body = {
         code: 200,
         message: "获取成功",
-        title: "抖音",
-        subtitle: "热榜",
+        ...routerInfo,
         total: cachedData.length,
         updateTime,
         data: cachedData,
@@ -125,12 +140,12 @@ douyinRouter.get("/douyin2/new", async (ctx) => {
       // 如果缓存中也没有数据，则返回错误信息
       ctx.body = {
         code: 500,
-        title: "抖音",
-        subtitle: "热榜",
+        ...routerInfo,
         message: "获取失败",
       };
     }
   }
 });
 
-module.exports = douyinRouter;
+douyinNewRouter.info = routerInfo;
+module.exports = douyinNewRouter;
