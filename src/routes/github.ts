212b58ada9cb1b8d975ac8944d32503c46eb6af4
helti <helti@qq.com>
2025-01-15 20:04:42 +0800

    fix: 导入路径

--- src/routes/github.ts
+++ src/routes/github.ts
@@ -2,8 +2,8 @@
 import fetch from "node-fetch";
 import * as cheerio from "cheerio";
 import { ListContext } from "../types";
-import logger from "../utils/logger";
-import {  getCache, setCache } from "../utils/cache";
+import logger from "../utils/logger.js";
+import {  getCache, setCache } from "../utils/cache.js";
 
 /**
  * 定义 Trending 仓库信息的类型
