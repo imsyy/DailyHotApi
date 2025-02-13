7059a24d481df7a011a8d0407b4190826ca2cf3b
helti <helti@qq.com>
2025-02-13 21:29:53 +0800

    feat: 虎扑增加股票区与分页参数

--- src/routes/hupu.ts
+++ src/routes/hupu.ts
@@ -4,7 +4,10 @@ import { get } from "../utils/getData.js";
 
 export const handleRoute = async (c: ListContext, noCache: boolean) => {
   const type = c.req.query("type") || "1";
-  const listData = await getList({ type }, noCache);
+  const page = c.req.query("page") || 1;
+  const pageSize = c.req.query("pageSize") || 20;
+  const cursor = c.req.query("cursor") || undefined; // 请求分页和分页参数必须带这个参数
+  const listData = await getList({ type, page, pageSize, cursor }, noCache);
   const routeData: RouterData = {
     name: "hupu",
     title: "虎扑",
@@ -18,6 +21,8 @@ export const handleRoute = async (c: ListContext, noCache: boolean) => {
           11: "校园区",
           12: "历史区",
           612: "摄影区",
+          21: "股票区",
+          10: "职场区",
         },
       },
     },
@@ -29,12 +34,13 @@ export const handleRoute = async (c: ListContext, noCache: boolean) => {
 };
 
 const getList = async (options: Options, noCache: boolean) => {
-  const { type } = options;
-  const url = `https://m.hupu.com/api/v2/bbs/topicThreads?topicId=${type}&page=1`;
+  const { type, page, pageSize, cursor } = options;
+  const url = `https://m.hupu.com/api/v2/bbs/topicThreads?topicId=${type}&page=${page}&pageSize=${pageSize}${cursor ? `&cursor=${cursor}` : ""}`;
   const result = await get({ url, noCache });
   const list = result.data.data.topicThreads;
   return {
     ...result,
+    nextCursor: result?.data?.data?.nextCursor,
     data: list.map((v: RouterType["hupu"]) => ({
       id: v.tid,
       title: v.title,
